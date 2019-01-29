6372612eedb0181cceb6c89a291af6f7409aa9ae
Brian Lagman <bjlagman@gmail.com>
2019-01-28 20:10:56 -0800

    Create functional watch button

--- src/components/MovieListEntry.jsx
+++ src/components/MovieListEntry.jsx
@@ -5,15 +5,29 @@ class MovieListEntry extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      watchStatus: 'Watch'
+      watchStatus: 'Watch',
     }
+    this.watched = this.watched.bind(this)
+  }
+  watched(e){
+    // console.log(this.state.watchStatus)
+    if(this.state.watchStatus === 'Watch'){
+      this.setState({watchStatus: 'Watched'})
+    }
+    if(this.state.watchStatus === 'Watched'){
+      this.setState({watchStatus: 'Watch'})
+    }
+
+    console.log('hi')
   }
   render(){
     return(
+      <div onClick={this.watched}>
       <div className='movie'>
         {this.props.movie.title}
-      <button className='watchToggle'>yay</button>
+      <button className='watchToggle'>{this.state.watchStatus}</button>
      </div>
+      </div>
     )
   }
 }
